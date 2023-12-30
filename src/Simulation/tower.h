18e9e962d853d069c3c8e650aa5a80b762b38472
Yanis <yanis.kernoua@epitech.eu>
2023-12-30 15:38:31 +0100

    feat(src/Simulation): Showing tower and their area

--- src/Simulation/tower.h
+++ src/Simulation/tower.h
@@ -8,11 +8,13 @@
 #pragma once
 #include <SFML/Audio/Types.h>
 #include <SFML/Graphics.h>
+#include <SFML/Graphics/Types.h>
 #define DEFAULT_TOWER_TEXTURE_PATH "src/assets/entity/tower.png"
 
 typedef struct tower_s {
     sfSprite *sprite;
     sfTexture *texture;
+    sfCircleShape *circle_area;
     sfVector2f position;
     float control_area;
 } tower_t;
